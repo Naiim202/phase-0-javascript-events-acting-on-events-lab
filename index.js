d29b874475aa6eac4339d667fa56d4c559de752f
hani <hani@DESKTOP-5I66P1U.localdomain>
2024-07-11 14:54:57 +0300

    events-acting-on -events-lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Your code here
+
+function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger');
+    let leftNumbers = dodger.style.left.replace('px', '');
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  
+  function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    let leftNumbers = dodger.style.left.replace('px', '');
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left < 360) { 
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
+  module.exports = {
+    moveDodgerLeft,
+    moveDodgerRight
+  };
+  
